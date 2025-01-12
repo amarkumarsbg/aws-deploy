dcd4b829491d54380c7f725425638663d991c189
Amar Kumar <amarkumarsbg13@gmail.com>
2025-01-12 17:10:01 +0530

    new api created

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 app.get("/api/get", (req, res) => {
   res.send({ message: "AWS Deployment" });
 });
+app.get("/api/get/user", (req, res) => {
+  res.send({ name: "Amar", email: "amar@gmail.com", role: "Developer" });
+});
 
 app.listen(5000, () => {
   console.log("Server is running on port: 5000");
